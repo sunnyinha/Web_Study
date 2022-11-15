1a73a441619692ec37d152a6525d2d07125498d8
sunny <lesunhe@inha.edu>
2022-11-16 00:15:18 +0900

    My code: Js variable base challenge

--- Javascript Base/app.js
+++ Javascript Base/app.js
@@ -1,36 +1,43 @@
-let age = 32;
-let userName = "Sunny";
-//**주의** 문자열 따옴표 사이 줄바꿈 금지(에러남) /해결 방법 1 : 문자열 내용이 길면 +로 끊어주기, 해결 방법 2: 따옴표 대신 역따옴표(키보드 1옆에 있음) 사용
-let talk = "very" + "nice";
-let expression = `good
-night`;
+// This is how a comment is added to JavaScript
+// Comments are not executed - they are just there to provide extra
+// information to you or other developers
 
-//배열 : 연관된 여러개의 값(숫자, 문자열 섞어도 됨)을 하나의 변수명으로 표현가능
-//배열의 인덱스 0부터 시작, 배열이름[인덱스]로 배열의 개별 항목 표현 가능
-let hobbies = ["Sports", "Cooking", "Reading"];
-alert(hobbies[0]);
+// Exercise Time!
 
-//객체: 연관이 없는 독립된 다양한 변수 모음(ex)여권 정보)
-//각각의 객체 요소들은 key(=변수,레이블) : vlaue 구성
-//객체 생성 시 각각의 key들은 객체에 scoped(소속) 된 상태이고 let 키워드를 통해 생성할 필요 x
-//각각 요소(key:value)별로 줄바꿈해주면 가독성 높음
-//객체 요소 접근 시 객체이름.(접근 객체 속성) 으로 접근 가능
+// 1) Create three new variables:
+//    - A variable that stores the name of an online course of your choice
+//    - A variable that stores the price of that course
+//    - A variable that stores the three main goals that you have, when taking this course
+let name = "Web online Course";
+let price = 1000;
+//배열 만들 때 대괄호 쓰기 (중괄호 아님)
+let goal = ["making my website", "making readable code", "Having Fun"];
 
-let job = {
-  title: "Developer",
-  place: "Korea",
-  salary: 0,
+// 2) Output ("alert") the three variable values
+alert(name);
+alert(price);
+alert(goal[0]);
+alert(goal[1]);
+alert(goal[2]);
+// 3) Try "grouping" the three variables together and still output their values thereafter
+// 객체 생성 할때 값을 넣어줘야 하니까 = 쓰는 거 까먹지 말기
+let grouping = {
+  name: "Web online Course",
+  price: 1000,
+  goal: ["making my website", "making readable code", "Having Fun"],
 };
-alert(job.place);
+// 4) Also output the second element in your "main goals" variable
+alert(grouping.goal[1]);
+// 5) Add a custom command that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
+function AccessGoal(g, th) {
+  return g[th];
+}
 
-//기본 연산
-let BMI;
-let weight = 60;
-let tall = 1.65;
-//덧셈
-tall = 1.65 + 0.05;
-//뺄셈
-weight = weight - 5;
-//곱셈& 나눗셈
-BMI = weight / (tall * tall);
-alert(BMI);
+// 6) Execute your custom command from (5) and output ("alert") the result
+alert(AccessGoal(grouping.goal, 2));
