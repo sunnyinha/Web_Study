243c1e130e2aefcea68b5a66b0a75c7dcef1ebb7
Sunny <lesunhe@gmail.com>
2022-12-29 22:38:45 +0900

    dom html element add, deleate, move, innerhtml

--- Dom/app.js
+++ Dom/app.js
@@ -7,3 +7,31 @@ anchorElement.href = "https://www.google.co.kr/";
 //anchorElement = document.querySelector("#external-id"); // CSS 선택자 여러개 존재 시 일치하는 첫번째 요소
 anchorElement = document.querySelectorAll("#external-id"); // CSS 선택자 여러개 존재 시 배열로 모두 받음
 anchorElement.href = "https://github.com/sunnyinha/Web_Study";
+
+// 요소 추가
+// 1. 새로운 요소 만들기
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://www.google.co.kr/";
+newAnchorElement.textContent = "This leads to Google!";
+// 2. 새로운 요소를 넣어줄 부모 요소 접근
+let firstParagraph = document.querySelector("p");
+// 3. 삽입해 줘야 할 요소에 접근
+firstParagraph.append(newAnchorElement);
+
+//요소 삭제
+//1. 빼야 할 요소 선택
+let firstH1Elenment = document.querySelector("h1");
+
+//2. 요소 삭제
+//firstH1Elenment.remove();
+firstH1Elenment.parentElement.removeChild(firstH1Elenment);
+
+//요소 이동
+firstParagraph.parentElement.append(firstParagraph);
+
+//innerHTML
+//html 요소까지 포함
+console.log(firstParagraph.innerHTML);
+//위의 것과 비교 텍스트 내용만
+//console.log(firstParagraph, textContent);
+firstParagraph.innerHTML = "Hi! This is <strong>important!</strong>";
