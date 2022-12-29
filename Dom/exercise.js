c4d8057bee6acaa572ade3d4f919aba9cacaab1e
Sunny <lesunhe@gmail.com>
2022-12-29 21:44:20 +0900

    My code : html access challenge

---
+++ Dom/exercise.js
@@ -0,0 +1,25 @@
+// Exercise Time!
+
+// 1. Select the <h1> element by "drilling into the DOM" and
+//    save it in a variable with a name of your choice
+let anchorElement = document.querySelector(h1);
+// 2. Use the variable from (1) and get access to the "parent"
+//    element of the stored <h1> element (i.e. to the <body> element)
+let p_anchorElement = anchorElement.parentElement();
+//    BONUS: Try using the variable from (1) to get access to the
+//    sibling element (i.e. the <p> element next to the <h1> element)
+let s_anchorElement = anchorElement.nextElementSibling();
+
+// 3. Select the <h1> element with getElementById and store in
+//    the same or a new variable (up to you)
+let m_anchorElement = anchorElement.getElementsById();
+
+// 4. Select the second <p> element with querySelector (you might
+//    need to add something in the HTML code, e.g. a class)
+//    and store it in a new variable with a name of your choice
+let Element = document.querySelectorAll(p);
+let second_Element = Element[1];
+
+// 5. BONUS TASK: Try changing the text content of the <p> element
+//    you selected in (4) and set it to any other text of your choice
+let text_Element = Element.prototype.TEXT_NODE();
