b9b6cec74d4aa3a7a3acbded8007c9ff4418e8eb
Sunny <lesunhe@gmail.com>
2023-01-05 22:34:30 +0900

    constrol stucture 1

---
+++ Dom/conditionall-code.js
@@ -0,0 +1,6 @@
+//if문 연습
+const myName = "Max";
+
+if (myName === "Max") {
+  console.log("Hello!");
+}
--- Dom/demo.js
+++ Dom/demo.js
@@ -1,18 +1,22 @@
-//const 적용! , 객체 고정과 값 고정의 차이 잘 이해하기
 const productNameInputElement = document.getElementById("product-name");
 const remainingCharsElement = document.getElementById("remaining-chars");
 
-//cosole.dir(productNameInputElement);
 const maxAllowedChars = productNameInputElement.maxLength; // maxLength 추출 최대글자수 유동적으로 반응
 
-// 함수에서는 실행할 때마다 메모리를 다시 받아오고 끝나면 날리고 하는 걸 이해하기
 function updateRemainingCharacters(event) {
   const enteredText = event.target.value;
   const enteredTextLength = enteredText.length;
 
-  //const remaingingCharacters = 60 - enteredTextLength;
   const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+  if (remainingCharacters <= 10) {
+    // 입력가능 글자수 10자 이하시 input 바탕 바뀌도록
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
+  }
 }
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
