90651dfcb477ca61cf0756d2fac8d7c80d1e0916
Sunny <lesunhe@gmail.com>
2023-01-05 18:22:52 +0900

    Provided code : count_words using dom

--- Dom/old-app.js
+++ Dom/old-app.js
@@ -1,13 +1,16 @@
-let inputElement = document.querySelector("input");
-function retrieveUserInput(event) {
-  let CountWord = event.target.value.length + "/ 60";
-  let pickspan = document.querySelector("span");
-  let newspan = document.createElement("span");
-  newspan.textContent = CountWord;
-  let pickbody = document.querySelector("body");
+let productNameInputElement = document.getElementById("product-name");
+let remainingCharsElement = document.getElementById("remaining-chars");
 
-  pickspan.remove(pickspan);
-  pickbody.append(CountWord);
-  // 에러남.. 1개까지만 글자수 세고 remove 처리 불가능
+//cosole.dir(productNameInputElement);
+let maxAllowedChars = productNameInputElement.maxLength; // maxLength 추출 최대글자수 유동적으로 반응
+
+function updateRemainingCharacters(event) {
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length;
+
+  //let remaingingCharacters = 60 - enteredTextLength;
+  let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+  remainingCharsElement.textContent = remainingCharacters;
 }
-inputElement.addEventListener("input", retrieveUserInput);
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
