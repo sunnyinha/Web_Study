8dcb371edae93f0207d6f39385ee443a2b64b7b1
Sunny <lesunhe@gmail.com>
2023-01-05 21:14:11 +0900

    constant apply

--- Dom/old-app.js
+++ Dom/old-app.js
@@ -1,15 +1,17 @@
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+//const 적용! , 객체 고정과 값 고정의 차이 잘 이해하기
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
 //cosole.dir(productNameInputElement);
-let maxAllowedChars = productNameInputElement.maxLength; // maxLength 추출 최대글자수 유동적으로 반응
+const maxAllowedChars = productNameInputElement.maxLength; // maxLength 추출 최대글자수 유동적으로 반응
 
+// 함수에서는 실행할 때마다 메모리를 다시 받아오고 끝나면 날리고 하는 걸 이해하기
 function updateRemainingCharacters(event) {
-  let enteredText = event.target.value;
-  let enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-  //let remaingingCharacters = 60 - enteredTextLength;
-  let remainingCharacters = maxAllowedChars - enteredTextLength;
+  //const remaingingCharacters = 60 - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
 }
