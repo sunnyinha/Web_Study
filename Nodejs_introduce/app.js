27b88ea98e8922e7bd9d29fcdec9f20587edd5c7
Sunny <lesunhe@gmail.com>
2023-02-04 20:18:40 +0900

    node js : basic respond

--- Nodejs_introduce/app.js
+++ Nodejs_introduce/app.js
@@ -1,8 +1,15 @@
 //node js 내장 모듈 http 불러오기
 const http = require("http");
 
+function handleRequest(requset, response) {
+  //statusCode : 요청 성공 여부
+  response.statusCode = 200;
+  //응답 준비 끝
+  response.end("<h1>Hello World!</h1>");
+}
+
 //서버 생성
-const server = http.createServer();
+const server = http.createServer(handleRequest);
 
 // 수신할 포트 설정
 //보통 80,443
