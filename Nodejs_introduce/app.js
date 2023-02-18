f91eb38e0d4beeb25062eea2835c742e454b1453
Sunny <lesunhe@gmail.com>
2023-02-19 00:26:17 +0900

    node js: install nodemon(auto save))

--- Nodejs_introduce/app.js
+++ Nodejs_introduce/app.js
@@ -1,4 +1,7 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const { json } = require("body-parser");
 
 const app = express();
 
@@ -11,7 +14,7 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    "<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>"
+    "<form action='/store-user' method='POST'><label>Your Name: </label><input type='text' name='username'><button>Submit</button></form>"
   );
 }); // localhost:3000/
 
@@ -19,8 +22,33 @@ app.post("/store-user", function (req, res) {
   //req.body : post 요청 데이터에 엑세스 가능
   //id가 아닌 name으로 acess
   const userName = req.body.username;
-  console.log(userName);
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+  existingUsers;
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
   res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+  responseData += "</ul>";
+
+  res.send(responseData);
 });
 
 app.listen(3000);
