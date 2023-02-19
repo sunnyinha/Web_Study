706a4f83599ce1485d9126ff7438097681595753
Sunny <lesunhe@gmail.com>
2023-02-19 19:44:50 +0900

    Get form post information

--- More express project/app.js
+++ More express project/app.js
@@ -1,10 +1,12 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
-//set static file
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -19,6 +21,20 @@ app.get("/restaurants", function (req, res) {
 app.get("/recommend", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", function (req, res) {
